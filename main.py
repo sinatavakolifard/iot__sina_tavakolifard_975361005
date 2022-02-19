688055f5ec10d6d799e40fe590d3e126c633afc6
Sina Tavakolifard <s.tavakolifard@gmail.com>
2022-02-19 22:25:08 +0330

    Ex1 part2

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ from random import randint
 nodes = []
 for id in range(1, 21):
     nodes.append(node(id, randint(0, 100), randint(0, 100)))
+
+
+# ex1 part2
+for i in range((len(nodes))):
+    temp_nodes = nodes[:]
+    del temp_nodes[i]
+    
+    j = 0
+    while j < 6:
+        id = randint(0, 18)
+        # print(id)
+        if temp_nodes[id] not in nodes[i].neighbor_nodes:
+            nodes[i].neighbor_nodes.append(temp_nodes[id])
+            j += 1
+
